58906060d2ca7b5b4ad61b99102ddec5bd90cc6e
Logan May <MAY16011454@btc.ac.uk>
2023-06-12 15:16:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import time
+import pyaudio
+import playsound
+import gtts from gTTS
+import openai
+import speech recognition as sr
+
+
+api_key = "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"
